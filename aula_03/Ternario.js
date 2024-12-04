2d6bc8787227c4b1dd9bf99aedf051363475c5b8
Madsik <jessica.rosario4@gmail.com>
2024-12-04 18:18:04 -0300

    Aula 03

---
+++ aula_03/Ternario.js
@@ -0,0 +1,6 @@
+let idade = 21;
+
+// Para simplificar um if simple da para usar o ternario
+
+idade > 18 ? console.log('O cliente é maior de 18 anos') : console.log('O cliente é menos de 18 anos')
+
